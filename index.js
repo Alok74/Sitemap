c619cfcc6e964aae366b717e38536569c16f6b5a
Alok Verma <alokverma0122@gmail.com>
2025-09-20 13:53:56 +0530

    j

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const linkRoutes= require("./routes/route")
+const dbConnect = require("./congfig/database")
 const PORT = process.env.PORT ||4000;
 
 app.use(express.json());
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Server connected successfully</h1>");
 });
 app.use("/edzy", linkRoutes);
-
+dbConnect();
 app.listen(PORT, () => {
   console.log(`Server is running at Port ${PORT}`);
 });
